24e0f76be1b6277fe6dc62f38e09ab9e734996ef
Kristiyan Michaylov <krisidm24@gmail.com>
2025-03-22 17:19:39 +0100

    Add database script

---
+++ backend/src/main/resources/db-script.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS Users (
+user_id INT NOT NULL UNIQUE,
+user_name VARCHAR(255),
+user_email VARCHAR(255) UNIQUE NOT NULL,
+user_password VARCHAR NOT NULL,
+PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS URL (
+short_url VARCHAR NOT NULL UNIQUE,
+original_url VARCHAR NOT NULL,
+user_id INT,
+creation TIMESTAMP,
+expiration TIMESTAMP,
+number_of_clicks INT,
+qr_code BYTEA,
+PRIMARY KEY (short_url),
+FOREIGN KEY (user_id) REFERENCES USERS(user_ID)
+    );
