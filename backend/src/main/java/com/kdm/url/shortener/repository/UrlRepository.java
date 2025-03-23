3b9c37c2a1c9c4f80af1859bdff798a032fbfbab
Kristiyan Michaylov <krisidm24@gmail.com>
2025-03-23 16:27:23 +0100

    Add Users and Url boilerplate repositories

--- backend/src/main/java/com/kdm/url/shortener/repository/UrlRepository.java
+++ backend/src/main/java/com/kdm/url/shortener/repository/UrlRepository.java
@@ -1,7 +1,9 @@
 package com.kdm.url.shortener.repository;
 
+import com.kdm.url.shortener.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class UrlRepository {
+public interface UrlRepository extends JpaRepository<Url, Long> {
+
 }
--- backend/src/main/java/com/kdm/url/shortener/repository/UsersRepository.java
+++ backend/src/main/java/com/kdm/url/shortener/repository/UsersRepository.java
@@ -1,4 +1,7 @@
 package com.kdm.url.shortener.repository;
 
-public class UsersRepository {
+import com.kdm.url.shortener.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<User, Long> {
 }
