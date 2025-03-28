b6986e5c38526bda2f96f6fa4d92cc01f4ee13cb
Kristiyan Michaylov <krisidm24@gmail.com>
2025-03-28 20:54:22 +0100

    Use Lombok Getter and Setter annotations

--- backend/src/main/java/com/kdm/url/shortener/dto/UrlDTO.java
+++ backend/src/main/java/com/kdm/url/shortener/dto/UrlDTO.java
@@ -1,9 +1,13 @@
 package com.kdm.url.shortener.dto;
 
 import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
+@Getter
+@Setter
 public class UrlDTO {
     private String shortUrl;
     private String originalUrl;
@@ -11,52 +15,4 @@ public class UrlDTO {
     private Timestamp expiration;
     private int numberOfClicks;
     private String qrCode;
-
-    public String getShortUrl() {
-        return shortUrl;
-    }
-
-    public void setShortUrl(String shortUrl) {
-        this.shortUrl = shortUrl;
-    }
-
-    public String getOriginalUrl() {
-        return originalUrl;
-    }
-
-    public void setOriginalUrl(String originalUrl) {
-        this.originalUrl = originalUrl;
-    }
-
-    public Timestamp getCreation() {
-        return creation;
-    }
-
-    public void setCreation(Timestamp creation) {
-        this.creation = creation;
-    }
-
-    public Timestamp getExpiration() {
-        return expiration;
-    }
-
-    public void setExpiration(Timestamp expiration) {
-        this.expiration = expiration;
-    }
-
-    public int getNumberOfClicks() {
-        return numberOfClicks;
-    }
-
-    public void setNumberOfClicks(int numberOfClicks) {
-        this.numberOfClicks = numberOfClicks;
-    }
-
-    public String getQrCode() {
-        return qrCode;
-    }
-
-    public void setQrCode(String qrCode) {
-        this.qrCode = qrCode;
-    }
 }
