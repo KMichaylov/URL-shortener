59649ffeae1c8b383fd17bbbe9aa6d41fb6cc450
Kristiyan Michaylov <krisidm24@gmail.com>
2025-03-28 20:53:30 +0100

    Add stubs for DTOs

---
+++ backend/src/main/java/com/kdm/url/shortener/dto/UrlDTO.java
@@ -0,0 +1,62 @@
+package com.kdm.url.shortener.dto;
+
+import jakarta.persistence.Column;
+
+import java.sql.Timestamp;
+
+public class UrlDTO {
+    private String shortUrl;
+    private String originalUrl;
+    private Timestamp creation;
+    private Timestamp expiration;
+    private int numberOfClicks;
+    private String qrCode;
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public Timestamp getCreation() {
+        return creation;
+    }
+
+    public void setCreation(Timestamp creation) {
+        this.creation = creation;
+    }
+
+    public Timestamp getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Timestamp expiration) {
+        this.expiration = expiration;
+    }
+
+    public int getNumberOfClicks() {
+        return numberOfClicks;
+    }
+
+    public void setNumberOfClicks(int numberOfClicks) {
+        this.numberOfClicks = numberOfClicks;
+    }
+
+    public String getQrCode() {
+        return qrCode;
+    }
+
+    public void setQrCode(String qrCode) {
+        this.qrCode = qrCode;
+    }
+}
