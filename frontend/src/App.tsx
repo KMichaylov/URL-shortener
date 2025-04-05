63c58a7d8b02334f6720d2e53989b8dcb995b1ce
Kristiyan Michaylov <krisidm24@gmail.com>
2025-04-05 19:11:45 +0200

    Add boilerplate code for Login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className="wrapper">
+                <div className="wrapper-registration">
+                    <CustomForm
+                        fields={loginConfig}
+                        onSubmit={handleFormSubmit}
+                        buttonConfig={buttonConfig}
+                        initialValues={{email: '', password: ''}}
+                    />
+                    <span>Don't have an account? <Link to="/register">Register here</Link></span>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
