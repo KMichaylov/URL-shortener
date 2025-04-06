f91be83dd71b495c23723413628525a2a70ccdf1
Kristiyan Michaylov <krisidm24@gmail.com>
2025-04-06 19:55:39 +0200

    Some style fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,23 @@
 import './App.css'
 import CustomForm from "./components/CustomForm.tsx";
+import {Flex, MantineProvider} from "@mantine/core";
 
 function App() {
 
     return (
-        <>
+        <MantineProvider>
             <div className="wrapper">
                 <div className="wrapper-registration">
                     <CustomForm
                         isLogin={true}
                     />
                 </div>
-                <span>Forgot the password?</span>
-                <span>Create an account</span>
-
+                <Flex>
+                    <span>Forgot the password?</span>
+                    <span>Create an account</span>
+                </Flex>
             </div>
-        </>
+        </MantineProvider>
     )
 }
 
