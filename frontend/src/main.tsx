a85ee5e8d163bbb3a11386eaecaabd62b621213d
Kristiyan Michaylov <krisidm24@gmail.com>
2025-04-12 00:25:10 +0200

    Add stub for router implementation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {BrowserRouter, createBrowserRouter, RouterProvider} from "react-router";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage.tsx";
+import MainPage from "./pages/MainPage/MainPage.tsx";
 
+const router = createBrowserRouter([
+    {
+        path: "/", element: <App/>
+    },
+    {
+        path: "/registration", element: <RegistrationPage/>
+    },
+    {
+        path: "/main", element: <MainPage/>
+    }
+])
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <RouterProvider router={router}/>
+    </BrowserRouter>,
 )
