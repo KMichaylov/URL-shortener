e55040a13c3862e1157098c04725e8e326667d10
Kristiyan Michaylov <krisidm24@gmail.com>
2025-04-18 23:48:17 +0200

    Fix button component

--- frontend/src/components/button/CustomButton.tsx
+++ frontend/src/components/button/CustomButton.tsx
@@ -1,17 +1,21 @@
-import {Button, MantineProvider} from "@mantine/core";
+import {Button, MantineProvider, MantineStyleProp} from "@mantine/core";
 import React from "react";
 
 type CustomButtonProps = {
     placeholder: string,
     onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
     className?: string;
+    radius?: string
+    size?: string
+    type?: "button" | "submit" | "reset" | undefined
+    style?: MantineStyleProp
 }
 
-const CustomButton: React.FC<CustomButtonProps> = ({placeholder, onClick, className}) => {
-    return (<MantineProvider>
-        <Button className={className} variant="light" color="cyan" size="md" radius="md"
+const CustomButton: React.FC<CustomButtonProps> = ({placeholder, onClick, className, radius, size, type, style}) => {
+    return (
+        <Button className={className} size={size} radius={radius} type={type} style={style}
                 onClick={onClick}>{placeholder}</Button>
-    </MantineProvider>)
+    )
 
 }
 
