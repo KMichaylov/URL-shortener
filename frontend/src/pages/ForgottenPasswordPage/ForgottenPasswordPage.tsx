c738a2594ad2139dea26c9abbec8dae3c588882c
Kristiyan Michaylov <krisidm24@gmail.com>
2025-04-19 17:53:18 +0200

    Add boilerplate for ForgottenPasswordPage

---
+++ frontend/src/pages/ForgottenPasswordPage/ForgottenPasswordPage.tsx
@@ -0,0 +1,26 @@
+import styles from './ForgottenPasswordPage.module.css'
+import {useNavigate} from "react-router";
+import CustomInput from "../../components/search/CustomInput.tsx";
+import React, {useState} from "react";
+import CustomButton from "../../components/button/CustomButton.tsx";
+import {Container, Title} from "@mantine/core";
+
+function ForgottenPasswordPage() {
+
+    const navigate = useNavigate()
+    const [email, setEmail] = useState<string>("");
+
+    // TODO: Add link for resetting the password
+    return (
+        <div className={styles["wrapper"]}>
+            <Container className={styles["items-container"]}>
+                <Title>Forgot Password</Title>
+                <CustomInput placeholder={"Enter email address"} value={email}
+                             onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)}/>
+                <CustomButton placeholder={'Reset Password'}/>
+            </Container>
+        </div>
+    )
+}
+
+export default ForgottenPasswordPage
