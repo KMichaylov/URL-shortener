58c4dbda0ae6d6e1a56f7cd706e12bedb57c3cdd
Kristiyan Michaylov <krisidm24@gmail.com>
2025-04-20 00:12:49 +0200

    Change some props of CustomInput.tsx

--- frontend/src/components/search/CustomInput.tsx
+++ frontend/src/components/search/CustomInput.tsx
@@ -2,7 +2,7 @@ import {Input, MantineProvider} from "@mantine/core";
 import React from "react";
 
 type CustomInputProps = {
-    name: string,
+    name?: string,
     label?: string,
     error?: string,
     description?: string,
