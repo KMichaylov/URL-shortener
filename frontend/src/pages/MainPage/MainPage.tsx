407d4aacec3824432ecad3ceca069145143ae723
Kristiyan Michaylov <krisidm24@gmail.com>
2025-04-20 23:30:47 +0200

    Add behaviour to MainPage.tsx

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,70 +1,102 @@
 import {Container, MantineProvider, Title, Text} from "@mantine/core";
-import CustomInput from "../../components/search/CustomInput.tsx";
+import CustomInput from "../../components/input/CustomInput.tsx";
 import CustomButton from "../../components/button/CustomButton.tsx";
 import CustomFooter from "../../components/footer/CustomFooter.tsx";
-import styles from "./MainPage.module.css"
+import styles from "./MainPage.module.css";
+import api from "../../api/apiConfiguration.ts";
 import {useState} from "react";
-import {Location, useLocation, useNavigate} from "react-router";
+import {useNavigate} from "react-router";
 
 function MainPage() {
-
-    const [value, setValue] = useState("")
-    const navigate: Location = useNavigate();
-    const location: Location = useLocation();
-
+    const [value, setValue] = useState("");
+    const navigate = useNavigate();
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setValue(event.target.value);
     };
 
-    const handleButtonClick = () => {
-        if (!value) {
-            // TODO: Add a message here
-            return;
+    // TODO: Move this common behaviour into separate file
+    const goToErrorPage = (title: string, message: string) => {
+        navigate("/error", {
+            state: {
+                title,
+                message
+            }
+        });
+    };
+
+    const shortenUrl = async () => {
+        try {
+            await api.post("/short-url", {originalUrl: value}, {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            navigate("/shorted-url");
+        } catch (error) {
+            console.log(error)
+            goToErrorPage(
+                "You have found a secret place.",
+                "Something went wrong when shortening the URL. Please check your link and try again!"
+            );
         }
+    };
 
-    }
-    console.log(styles)
-    return (<MantineProvider>
-        <div className={styles["wrapper-main"]}>
-            <Container>
-                <Title order={1}>Short URL</Title>
-            </Container>
+    const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        if (value.trim()) {
+            shortenUrl();
+        } else {
+            goToErrorPage(
+                "You have found a secret place.",
+                "You have most probably misspelled the link or provided an empty link. Please go back to the main page and try again!"
+            );
+        }
+    };
 
-            <div className={styles["wrapper-urlbox"]}>
-                <Title order={2}>Paste the URL you want to shorten</Title>
-                <div className={styles["input-button-row"]}>
-                    <div className={styles["input"]}>
-                        <CustomInput
-                            placeholder={"Enter your link here"}
-                            name={"url-input"}
-                            value={value}
-                            onChange={handleInputChange}
-                            className={"input-field"}
+
+    return (
+        <MantineProvider>
+            <div className={styles["wrapper-main"]}>
+                <Container>
+                    <Title order={1}>Short URL</Title>
+                </Container>
+
+                <div className={styles["wrapper-urlbox"]}>
+                    <Title order={2}>Paste the URL you want to shorten</Title>
+                    <div className={styles["input-button-row"]}>
+                        <div className={styles["input"]}>
+                            <CustomInput
+                                placeholder={"Enter your link here"}
+                                name={"url-input"}
+                                value={value}
+                                onChange={handleInputChange}
+                                className={"input-field"}
+                            />
+                        </div>
+                        <CustomButton
+                            placeholder={"Shorten URL"}
+                            onClick={handleButtonClick}
                         />
                     </div>
-                    <CustomButton
-                        placeholder={"Shorten URL"}
-                        onClick={handleButtonClick}
-                    />
+                    <Text size="sm">
+                        This is a free tool to shorten URLs and generate short links.
+                        URL shortener allows you to create a shortened link making it easy to share.
+                    </Text>
                 </div>
-                <Text size="sm">
-                    This is a free tool to shorten URLs and generate short links.
-                    URL shortener allows you to create a shortened link making it easy to share.
-                </Text>
-            </div>
 
-            <div className={styles["wrapper-information"]}>
-                <Title order={2}>Shorten, share and analyse</Title>
-                <Text size="md">
-                    Your shortened URLs can be used in various locations without taking much space.
-                    See how many times your link has been clicked using the click counter!
-                </Text>
-            </div>
+                <div className={styles["wrapper-information"]}>
+                    <Title order={2}>Shorten, share and analyse</Title>
+                    <Text size="md">
+                        Your shortened URLs can be used in various locations without taking much space.
+                        See how many times your link has been clicked using the click counter!
+                    </Text>
+                </div>
 
-            <CustomFooter/>
-        </div>
-    </MantineProvider>)
+                <CustomFooter/>
+            </div>
+        </MantineProvider>
+    );
 }
 
-export default MainPage
+export default MainPage;
