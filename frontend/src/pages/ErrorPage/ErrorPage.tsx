ef7178b41d069e5e6f31f9a7c874e76aeb047bed
Kristiyan Michaylov <krisidm24@gmail.com>
2025-04-20 23:30:14 +0200

    Add ErrorPage.tsx

--- frontend/src/pages/ErrorPage/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,22 +1,26 @@
 import {Button, Container, Group, Text, Title} from '@mantine/core';
 import classes from './ErrorPage.module.css';
+import {useLocation, useNavigate} from 'react-router';
 
-type ErrorProps = {
-    code: string;
-};
+function ErrorPage() {
+    const navigate = useNavigate();
+    const location = useLocation();
 
-function ErrorPage({code}: ErrorProps) {
+    const {title, message} = location.state || {
+        title: "Unknown error",
+        message: "Something went wrong. Please try again."
+    };
+
     return (
         <Container className={classes.root}>
-            <div className={classes.label}>{code}</div>
-            <Title className={classes.title}>You have found a secret place.</Title>
+            <div className={classes.label}>Oops</div>
+            <Title className={classes.title}>{title}</Title>
             <Text c="dimmed" size="lg" ta="center" className={classes.description}>
-                Unfortunately, this is only a {code} page. You may have mistyped the address, or the page has
-                been moved to another URL.
+                {message}
             </Text>
             <Group justify="center">
-                <Button variant="subtle" size="md">
-                    Take me back to home page
+                <Button variant="subtle" size="md" onClick={() => navigate("/main")}>
+                    Go Back
                 </Button>
             </Group>
         </Container>
