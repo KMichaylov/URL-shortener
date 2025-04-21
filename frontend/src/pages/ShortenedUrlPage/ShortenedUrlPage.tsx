8a6e8dcd7f46e1c1f058c9b5e4b9e84131f3617a
Kristiyan Michaylov <krisidm24@gmail.com>
2025-04-22 00:18:26 +0200

    Add functionality to ShortenedUrlPage.tsx

--- frontend/src/pages/ShortenedUrlPage/ShortenedUrlPage.tsx
+++ frontend/src/pages/ShortenedUrlPage/ShortenedUrlPage.tsx
@@ -1,31 +1,37 @@
-import {MantineProvider, Text, Title} from "@mantine/core";
-import CustomInput from "../../components/search/CustomInput.tsx";
+import {Text, Title} from "@mantine/core";
+import CustomInput from "../../components/input/CustomInput.tsx";
 import CustomButton from "../../components/button/CustomButton.tsx";
-import {useState} from "react";
-import {Location, useLocation, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router";
 import ButtonCopy from "../../components/button/CopyButton.tsx";
 import CustomFooter from "../../components/footer/CustomFooter.tsx";
 import styles from "./ShortenedUrlPage.module.css"
-
+import api from "../../api/apiConfiguration.ts";
+import ErrorPage from "../ErrorPage/ErrorPage.tsx";
 
 function ShortenedUrlPage() {
 
     const [value, setValue] = useState("")
-    const navigate: Location = useNavigate();
-    const location: Location = useLocation();
+    const navigate = useNavigate();
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(event.target.value);
-    };
+    const [hasError, setHasError] = useState(false);
 
-    const handleButtonClick = () => {
-        if (!value) {
-            // TODO: Add a message here
-            return;
-        }
+    useEffect(() => {
+        api.get("/clicks")
+            .then((res) => setValue(res.data))
+            .catch(() => setHasError(true));
+    }, []);
 
+
+    const handleButtonCountClick = () => {
+        navigate("/shorted-url/clicks")
     }
 
+    const handleBackButtonClick = () => {
+        navigate("/main")
+    }
+
+    if (hasError) return <ErrorPage/>;
     return (
         <div className={styles["wrapper-main"]}>
             <div className={styles["title"]}>
@@ -34,14 +40,13 @@ function ShortenedUrlPage() {
             </div>
             <div className={styles["wrapper-urlbox"]}>
                 <div className={styles["input-button-row"]}>
-                    <CustomInput placeholder={"Enter URL"} name={"url-input"} value={value}
-                                 onChange={handleInputChange}/>
-                    <ButtonCopy shortenedValue={handleInputChange}/>
+                    <CustomInput placeholder={"Enter URL"} name={"url-input"} value={value}/>
+                    <ButtonCopy shortenedValue={value}/>
                 </div>
                 <div className={styles["url-info"]}>
                     <Text className={styles["original-text"]}>Original URL: ...</Text>
-                    <CustomButton placeholder={"Total number of clicks"} onClick={handleButtonClick}/>
-                    <CustomButton placeholder={"Go back"} onClick={handleButtonClick}/>
+                    <CustomButton placeholder={"Total number of clicks"} onClick={handleButtonCountClick}/>
+                    <CustomButton placeholder={"Go back"} onClick={handleBackButtonClick}/>
                     <CustomFooter/>
                 </div>
             </div>
