3eaefe881944b952063b033a781d9552efbc5da9
Kristiyan Michaylov <krisidm24@gmail.com>
2025-04-27 01:31:06 +0200

    Add SecurityConfig

--- backend/src/main/java/com/kdm/url/shortener/config/SecurityConfig.java
+++ backend/src/main/java/com/kdm/url/shortener/config/SecurityConfig.java
@@ -1,15 +1,30 @@
 package com.kdm.url.shortener.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/urls/**").authenticated()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
     }
 
 }
