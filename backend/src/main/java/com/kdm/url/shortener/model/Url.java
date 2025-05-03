c4eb32cdca3f4cf4f1445ba217b312136380c348
Kristiyan Michaylov <krisidm24@gmail.com>
2025-05-03 23:24:02 +0200

    Add validation logic for both models

--- backend/src/main/java/com/kdm/url/shortener/model/Url.java
+++ backend/src/main/java/com/kdm/url/shortener/model/Url.java
@@ -3,6 +3,8 @@ package com.kdm.url.shortener.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.annotations.Cascade;
 
 import java.sql.Timestamp;
@@ -14,23 +16,27 @@ import java.sql.Timestamp;
 public class Url {
 
     @Id
+    @NotBlank(message = "Short URL cannot be blank")
     @Column(name = "short_url")
     private String shortUrl;
 
+    @NotBlank(message = "Original URL cannot be blank")
     @Column(name = "original_url")
     private String originalUrl;
 
+    @NotNull(message = "User must not be null")
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private User user;
 
+    @NotNull(message = "Creation timestamp cannot be null")
     @Column(name = "creation")
     private Timestamp creation;
 
+    @NotNull(message = "Expiration timestamp cannot be null")
     @Column(name = "expiration")
     private Timestamp expiration;
 
     @Column(name = "number_of_clicks")
     private int numberOfClicks;
-
 }
--- backend/src/main/java/com/kdm/url/shortener/model/User.java
+++ backend/src/main/java/com/kdm/url/shortener/model/User.java
@@ -1,6 +1,7 @@
 package com.kdm.url.shortener.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,16 +18,25 @@ public class User {
     @Column(name = "user_id")
     private Long userId;
 
+    @NotBlank(message = "Name is mandatory")
+    @Pattern(regexp = "^[A-Za-z' ]+$", message = "Username must contain only letters and apostrophes.")
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
     @Column(name = "user_name")
     private String name;
 
-    @Column(name = "user_email")
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Invalid email format")
+    @Column(name = "user_email", unique = true)
     private String email;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Url> urls;
 
+    @NotBlank(message = "Password is mandatory")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d!@#$%^&*()_+~\\-={}\\[\\]:;\"'<>,.?/]{10,}$",
+            message = "Password must be at least 10 characters, include uppercase, lowercase, and a number."
+    )
     @Column(name = "user_password")
     private String password;
-
 }
