539ad84e23b0346761d12529e29d5c739994e317
Kristiyan Michaylov <krisidm24@gmail.com>
2025-05-06 00:31:53 +0200

    Add JWT token to login and registration

--- frontend/src/api/apiConfiguration.ts
+++ frontend/src/api/apiConfiguration.ts
@@ -1,6 +1,21 @@
-import axios from 'axios'
+import axios, {AxiosInstance} from 'axios'
+
 const BASE_URL: string = 'http://localhost:8080/api';
 
-export default axios.create({
+const axiosObject: AxiosInstance = axios.create({
     baseURL: BASE_URL
 })
+
+axiosObject.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('jwtToken');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+)
+
+export default axiosObject
+
--- frontend/src/components/form/CustomForm.tsx
+++ frontend/src/components/form/CustomForm.tsx
@@ -11,6 +11,7 @@ import {validateEmail, validatePassword, validateUsername} from '../../services/
 import {createHandleChange} from "../../utils/formHelper.ts";
 import {Errors} from "../../types/Errors.ts";
 import {FormValues} from "../../types/FormValues.ts";
+import baseUrl from "../../api/apiConfiguration.ts";
 
 type CustomFormProps = {
     isLogin: boolean;
@@ -44,12 +45,38 @@ const CustomForm: React.FC<CustomFormProps> = ({isLogin}) => {
     const [errors, setErrors] = useState<Errors>({});
     const navigate = useNavigate();
 
-    const handleSubmit = (e: FormEvent) => {
+    const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
         const validationErrors = validateForm(formValues, isLogin);
         setErrors(validationErrors);
         if (Object.keys(validationErrors).length === 0) {
-            navigate('/main');
+            const endpoint = isLogin ? '/users/login' : '/users/registration';
+            const payload = isLogin
+                ? {
+                    email: formValues.email,
+                    password: formValues.password,
+                }
+                : {
+                    email: formValues.email,
+                    password: formValues.password,
+                    username: formValues.username,
+                };
+
+            baseUrl
+                .post(endpoint, payload)
+                .then((response) => {
+                    const responseData = response.data as string;
+                    const token = responseData
+                        .replace(/^Bearer\s/, '')
+                        .replace(/^User registered successfully! Token:\s/, '');
+
+                    localStorage.setItem('jwtToken', token);
+                    navigate('/main');
+                })
+                .catch((error) => {
+                    console.error("An error occurred in the process", error);
+                    alert("An error occurred. Please check your credentials or try again later.");
+                });
         }
     };
 
