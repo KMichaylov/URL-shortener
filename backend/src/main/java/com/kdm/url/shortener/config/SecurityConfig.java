930b5e9ebd993f0a2b6c9d392314101cb675671e
Kristiyan Michaylov <krisidm24@gmail.com>
2025-05-08 00:29:02 +0200

    Update cors setting and add configurer

--- backend/src/main/java/com/kdm/url/shortener/config/SecurityConfig.java
+++ backend/src/main/java/com/kdm/url/shortener/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.kdm.url.shortener.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/users/login", "/api/users/registration").permitAll()
