9103ca5f51480de2a4692f4e74ddfe1fcd87bc64
Krisi <krisidm24@gmail.com>
2025-07-20 01:00:30 +0300

    Add UrlRequest dto

---
+++ backend/src/main/java/com/kdm/url/shortener/dto/UrlRequest.java
@@ -0,0 +1,13 @@
+package com.kdm.url.shortener.dto;
+
+public class UrlRequest {
+    private String originalUrl;
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+}
